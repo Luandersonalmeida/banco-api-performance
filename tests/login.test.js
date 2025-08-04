3845ae49978bcae602665c071b0ee1fffb5f2df7
Luanderson Almeida <ls_luanderson@hotmail.com>
2025-08-03 21:08:31 -0300

    trabalhando com stages

--- tests/login.test.js
+++ tests/login.test.js
@@ -4,13 +4,20 @@ import { sleep, check } from 'k6';
 const config = JSON.parse(open('../config.json'));
 
 export  const options = {
-    vus: 10,
-    duration: '30s',
-    thresholds: {
+    stages: [
+        { duration: '10s', target: 10 },
+        { duration: '20s', target: 10 },
+        { duration: '10s', target: 30 },
+        { duration: '20s', target: 30 },
+        { duration: '20s', target: 0 },
+    ],
+      
+thresholds: {
       http_req_duration: ['p(90)<3000', 'max<6000'],
       http_req_failed: ['rate<0.01']
-    },
-
+    
+    }
+    
 }
 
 export default function () {
